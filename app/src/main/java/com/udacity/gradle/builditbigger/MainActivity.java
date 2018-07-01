89abe2c66fa92a42cfb1bdfcafd98003483a0135
Maxime Gaj <maxime.gaj@gmail.com>
2018-07-01 16:33:57 +0200

    completes step 1

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.udacity.maxgaj.builditbigger.jokesjavalib.JokeProvider;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        JokeProvider jokeProvider = new JokeProvider();
+        String joke = jokeProvider.tellAJoke();
+        Toast.makeText(this, joke, Toast.LENGTH_LONG).show();
     }
 
 
---
+++ jokesJavaLib/src/main/java/com/udacity/maxgaj/builditbigger/jokesjavalib/JokeProvider.java
@@ -0,0 +1,7 @@
+package com.udacity.maxgaj.builditbigger.jokesjavalib;
+
+public class JokeProvider {
+    public String tellAJoke(){
+        return "this is a funny joke!";
+    }
+}
